7a70336fab36a61583a385d81a675ad6bf866d5e
kellyeryan <kellyeryan@gmail.com>
2020-04-27 11:49:43 -0400

    Attach link to classgroups page

---
+++ app/javascript/components/ClassGroupsIndexPage.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+function ClassGroupsIndexPage(props) {
+    console.log(props)
+
+    return (
+        <>
+            <div>
+                {
+                    props.groups.map((classroom, index) => {
+                        return <ul key={index}>{classroom.name}</ul>
+                    })
+                }
+            </div>
+            <button
+                type="button"
+                onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = '/class_groups/new';
+                }}
+            > Click here</button>
+        </>
+    )
+}
+export default ClassGroupsIndexPage
